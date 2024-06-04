340ed9be9c3d6c349a560bf8e1b84ea870b5766f
Riley Kiltz <Riley.kiltz@kellogg.northwestern.edu>
2024-06-04 03:08:54 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'places#index'
-  
+
   resources "entries"
   resources "places"
-  resources "sessions", only: [:create]
-  resources "users"
-  get '/places', to: 'places#index'
+  resources "sessions", only: [:new, :create]
+  resources "users", only: [:new, :create]
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  # Other routes...
+  
 end
